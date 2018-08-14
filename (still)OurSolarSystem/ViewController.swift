e98c9fea518e98632b19a06872dcec23fc7806b6
Jakub Slawecki <jakubslawecki@gmail.com>
2018-08-14 18:00:15 +0200

    AR Configuration, icons etc

--- (still)OurSolarSystem/ViewController.swift
+++ (still)OurSolarSystem/ViewController.swift
@@ -7,19 +7,24 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
-
+    let configuration = ARWorldTrackingConfiguration()
+    
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.session.run(configuration)
+
+       
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+   
 
 }
 
